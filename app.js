3626e22974d114a9a5ff16ac66491452234e84c5
Daniel <danieltsegay2286@gmail.com>
2025-01-06 16:25:13 +0300

    Fix: Bind server to dynamic PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const dbConnection = require("./db/dbConfig");
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 const cors = require("cors")
 
 app.use(cors())
